6f31e5690c569dbe6216ac10ca1ec33092c7cd63
gitSuki <collinbrown226@gmail.com>
2021-12-30 20:38:09 -0600

    added CSS layour

--- script.js
+++ script.js
@@ -13,3 +13,27 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b
 }
+
+function exponent(a, b) {
+    return a ** b
+}
+
+function operate(a, b, operator) {
+    switch(operator) {
+        case 'addition':
+            return add(a,b);
+
+        case 'subtraction':
+            return subtract(a,b)
+
+        case 'multiplication':
+            return multiply(a,b)
+
+        case 'division':
+            return divide(a,b)
+
+        case 'power':
+            return exponent(a,b)
+    }
+}
+
